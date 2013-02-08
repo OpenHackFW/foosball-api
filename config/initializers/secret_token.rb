c54bf1bd70e3ceb1e05c8c94635a9ed2e7a24150
Morgan Showman <mshowman@squaremouth.com>
2013-02-07 19:35:10 -0500

    Install rails skipping active record

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoosballApi::Application.config.secret_token = '62e250195788ca9131b8ce2033b5c834d0166b82db7ae5336e21b3e7c6dc741a0b1b9e57446542996d4ddae33debf1f2622ae9158170f2677e564d45bf07b8ab'
